655d553a7bd4372496ad52c6d4cb9bbf55016f64
Micah Silverman <micah@afitnerd.com>
2016-02-16 16:06:03 -0500

    Updated test to support Spring Security and webapp

--- src/test/java/com/stormpath/tutorial/ApplicationTests.java
+++ src/test/java/com/stormpath/tutorial/ApplicationTests.java
@@ -1,17 +1,28 @@
 package com.stormpath.tutorial;
 
 
+import com.stormpath.tutorial.config.SpringSecurityWebAppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringApplicationConfiguration(classes = {Application.class, SpringSecurityWebAppConfig.class})
+@EnableWebSecurity
+@WebAppConfiguration
 public class ApplicationTests {
+    @Autowired
+    com.stormpath.sdk.application.Application application;
 
 	@Test
 	public void contextLoads() {
+        assertEquals("My Application", application.getName());
 	}
 
 }
